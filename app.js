fe4eaf644beaee0b8c66e53101e43f96c53b3fe9
smoockpp <smoockpp@gmail.com>
2017-01-18 20:35:51 +0000

    adding middleware, login and profile views

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 var app = express();
+
+
 
 
 // mongodb connection
@@ -9,6 +13,24 @@ mongoose.connect("mongodb://localhost:27017/bookworm");
 var db = mongoose.connection;
 //mongo err
 db.on('error', console.error.bind(console, 'connection error: '));
+
+//use session for tracking logins
+app.use(session({
+  secret: 'treehouse loves you',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+// make user ID available in templates
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.session.userId;
+  // console.log(res.locals.currentUser);
+  next();
+});
+
 
 // parse incoming requests
 app.use(bodyParser.json());
--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 var UserSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -20,6 +21,41 @@ var UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   }
+});
+
+// authenticate input against database documents
+UserSchema.statics.authenticate = function(email, password, callback) {
+  User.findOne({email: email})
+    .exec(function(error, user) {
+      if(error) {
+        return callback(error);
+      } else if ( !user ) {
+        var err = new Error('User not found');
+        err.status = 401;
+        return callback(err);
+      }
+      bcrypt.compare(password, user.password, function(error, result) {
+        if ( result === true) {
+          return callback(null, user);
+        } else {
+          return callback();
+        }
+      });
+    });
+};
+
+
+// hash password before saving to db
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function(err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  });
 });
 
 var User = mongoose.model('User', UserSchema);
